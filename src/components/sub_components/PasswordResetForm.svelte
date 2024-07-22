6c0bbe44c2f100aba10df08fb587e84812a3b80c
Michael Obeng <msquireobeng@gmail.com>
2024-07-22 14:48:15 +0000

    password reset

--- src/components/sub_components/PasswordResetForm.svelte
+++ src/components/sub_components/PasswordResetForm.svelte
@@ -1,30 +1,14 @@
 <script>
-    import { goto } from '$app/navigation'
-    import { show_login_form, show_register_form, email, password, show_registration_success } from '$lib/store.js'
+    import { email } from '$lib/store.js'
     let show_email_login_error = false;
-    let show_password_login_error =  false;
+    let submitting_password_reset = false;
     export let pb;
-    export let user;
 
-    let submitting_login = false;
-
-    if(user?.verified) {
-        goto('/home')
-    }
-
-    const go_to_register_form = () => {
-        show_login_form.set(false)
-        show_register_form.set(true)
-    }
     const set_email = (e) => {
         email.set(e.target.value)
         validate_email(e)
     }
-    const set_password = (e) => {
-        password.set(e.target.value)
-    }
 
-    // Validation
     const validate_email = (e) => {
         let email_pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if (!email_pattern.test(e.target.value)) {
@@ -33,32 +17,29 @@
             show_email_login_error = false
         }
     }
+    const reset_password = async () => {
 
-    const login = async () => {
+        submitting_password_reset = true
+
         try {
-            submitting_login = true
-            const authData = await pb.collection('merchants').authWithPassword(
-                $email,
-                $password,
-            );
-        
-            if(user.verified) {
-                goto('/home')
-            } else {
-                show_login_form.set(false)
-                show_registration_success.set(true)
-            }
+            await pb.collection('merchants').requestPasswordReset($email);
         } catch (error) {
             console.log(error)
         } finally {
-            submitting_login = false
+            submitting_password_reset = false;
+            setTimeout(() => {
+                show_password_reset_form.set(false)
+                show_register_form.set(false)
+                show_registration_success.set(false)
+                show_login_form.set(true)
+            }, 3000)
         }
 
     }
 </script>
 
 <style lang="postcss">
-    .login_form {
+    .password_reset_form {
         @apply px-4 mt-8;
     }
     .form_item {
@@ -67,53 +48,17 @@
     .form_item label {
         @apply pl-1 mb-1.5 text-sm font-semibold;
     }
-    .form_item #email, .form_item #password {
+    .form_item #email {
         @apply border-2 border-border_grey py-3 px-4 rounded-md focus:outline-accent_bg;
     }
-    .email_login_error, .password_login_error {
+    .email_reset_error {
         @apply mt-2 border border-red bg-red/10 rounded-md px-4 py-2.5;
     }
-    .email_error_text, .password_error_text {
+    .email_reset_error_text {
         @apply text-red text-sm;
     }
     .form_item.submit_btn {
-        @apply bg-accent_bg py-4 rounded-md text-white font-bold text-lg cursor-pointer flex justify-center items-center;
-    }
-    .form_item.submit_btn {
-        @apply mt-2;
-    }
-    .register_redirect {
-        @apply flex items-center justify-center;
-    }
-    .register_redirect .register_redirect_text {
-        @apply text-sm;
-    }
-    .register_redirect .register_redirect_link {
-        @apply text-accent_bg font-medium cursor-pointer;
-    }
-    .login_divider {
-        @apply flex flex-row justify-center items-center mt-6 px-8;
-    }
-    .login_divider span {
-        @apply text-nowrap mx-3 text-border_grey font-semibold text-sm;
-    }
-    .login_divider hr {
-        @apply w-[-webkit-fill-available] border border-border_grey;
-    }
-    .social_login {
-        @apply flex flex-row justify-center items-center mt-6 gap-8;
-    }
-    .social_login_link {
-        @apply h-14 w-14 p-2.5 rounded-lg cursor-pointer;
-    }
-    .social_login_link img {
-        @apply h-full w-full;
-    }
-    .facebook, .twitter {
-        @apply bg-accent_bg/20;
-    }
-    .google {
-        @apply bg-highlight_bg/20;
+        @apply bg-accent_bg py-4 mt-2 rounded-md text-white font-bold text-lg cursor-pointer flex justify-center items-center;
     }
 
     /* Button loader */
@@ -209,7 +154,7 @@
     }
 </style>
 
-<form class="login_form">
+<form class="password_reset_form">
     <div class="form_item">
         <label for="email">Email</label>
         <input 
@@ -221,23 +166,12 @@
             on:blur={validate_email}
         />
         {#if show_email_login_error}
-            <div class="email_login_error">
-                <span class="email_login_error_text">Looks like you haven't entered a valid email.</span>
+            <div class="email_reset_error">
+                <span class="email_reset_error_text">Looks like you haven't entered a valid email.</span>
             </div>
         {/if}
     </div>
-    <div class="form_item">
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Enter your password" on:input={set_password}/>
-        {#if show_password_login_error}
-            <div class="password_login_error">
-                <span class="password_login_error_text">This password is incorrect</span>
-            </div>
-        {/if}
-    </div>
-
-
-    {#if submitting_login}
+    {#if submitting_password_reset}
             <div class="form_item submit_btn">
                 <div class="loader">
                     <div class="bar1"></div>
@@ -255,25 +189,8 @@
                 </div>
             </div>
     {:else}
-        <div class="form_item submit_btn" on:click={login}>
-            <span>Login</span>
+        <div class="form_item submit_btn" on:click={reset_password}>
+            <span>Send password reset email</span>
         </div>
     {/if}
-    <div class="register_redirect">
-        <p class="register_redirect_count">Don't have an account? <span class="register_redirect_link" on:click={go_to_register_form}>Register</span></p>
-    </div>
-    <div class="login_divider">
-        <hr/><span>Or Login With</span><hr/>
-    </div>
-    <div class="social_login">
-        <div class="social_login_link facebook">
-            <img src="/images/facebook.png" />
-        </div>
-        <div class="social_login_link google">
-            <img src="/images/google.png" />
-        </div>
-        <div class="social_login_link twitter">
-            <img src="/images/twitter.png" />
-        </div>
-    </div>
 </form>
--- src/routes/(auth)/auth/+page.svelte
+++ src/routes/(auth)/auth/+page.svelte
@@ -10,6 +10,14 @@
 	import RegisterForm from '../../../components/sub_components/RegisterForm.svelte';
     import { pocketbase, currentUser } from '../../../lib/pocketbase';
 	import RegistrationSuccess from '../../../components/sub_components/RegistrationSuccess.svelte';
+	import PasswordResetForm from '../../../components/sub_components/PasswordResetForm.svelte';
+
+    const back_to_login = () => {
+        show_password_reset_form.set(false)
+        show_register_form.set(false)
+        show_registration_success.set(false)
+        show_login_form.set(true)
+    }
 </script>
 
 <style lang="postcss">
@@ -32,6 +40,22 @@
             <LoginForm 
                 pb={pocketbase}
                 user={$currentUser}
+            />
+        </div>
+    {/if}
+    {#if $show_password_reset_form}
+        <div class="password_reset_form_wrapper" transition:slide={{ delay: 250, duration: 300, easing: quintOut, axis: 'x' }}>
+            <GenericHeading 
+                page_title={'Password reset'}
+                back_btn
+                back_to_prev={back_to_login}
+            />
+            <AuthGreeting 
+                title={'Password reset'}
+                subtitle={'If you\'ve forgotten your password, please enter your email address below.  We will promptly send you a link to reset your password.'}
+            />
+            <PasswordResetForm
+                pb={pocketbase}
             />
         </div>
     {/if}
